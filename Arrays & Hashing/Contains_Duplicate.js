712b6cd107305a0c6b2d1ae069f7ce77f5a6b77a
ilyasbikarnaf <154575394+ilyasbikarnaf@users.noreply.github.com>
2025-03-10 22:02:44 +0000

    add 2 more solutions for 'contains duplicate'

--- Arrays & Hashing/Contains_Duplicate.js
+++ Arrays & Hashing/Contains_Duplicate.js
@@ -9,3 +9,40 @@ class Solution {
     return numsSet.size !== nums.length;
   }
 }
+
+//2nd solution
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    const numsSet = new Set();
+    for (const num of nums) {
+      if (numsSet.has(num)) return true;
+      else {
+        numsSet.add(num);
+      }
+    }
+    return false;
+  }
+}
+
+// 3rd solution
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    const obj = {};
+
+    for (const num of nums) {
+      if (obj[num]) return true;
+      else {
+        obj[num] = 1;
+      }
+    }
+    return false;
+  }
+}
